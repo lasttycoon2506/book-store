aef1b780e6720731f197427849036e919b3f2288
William <robertw5@oregonstate.edu>
2024-01-11 14:27:39 -0500

    .env init placement

--- server/src/app.js
+++ server/src/app.js
@@ -4,15 +4,15 @@ const app = express()
 const morgan = require("morgan")
 app.use(morgan('dev'))
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const dynamoose = require("dynamoose")
 dynamoose.aws.sdk.config.update({
     "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
     "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
     "region": "us-east-1",
 });
-
-const dotenv = require('dotenv');
-dotenv.config();
 
 app.use(express.json())
 app.use(require("./routes"))
