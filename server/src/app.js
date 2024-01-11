9d2f7d00e1044ae533a87d57d52d848f2f52fd3b
William <robertw5@oregonstate.edu>
2024-01-11 14:03:28 -0500

    connect aws cli env

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,20 @@
 const express = require('express')
-const morgan = require("morgan")
-
 const app = express()
 
-app.use(express.json())
-const dynamoose = require("dynamoose")
-dynamoose.aws.sdk.config.update({"region": "us-east-2"});
+const morgan = require("morgan")
 app.use(morgan('dev'))
+
+const dynamoose = require("dynamoose")
+dynamoose.aws.sdk.config.update({
+    "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+    "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+    "region": "us-east-1",
+});
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(express.json())
 app.use(require("./routes"))
 
 
