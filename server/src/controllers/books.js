99464375fd608000fb99ed1fa1a1e41e06025140
William <robertw5@oregonstate.edu>
2024-01-11 09:59:25 -0500

    books controller

--- server/src/controllers/books.js
+++ server/src/controllers/books.js
@@ -1,13 +1,9 @@
 const router = require("express").Router()
 
-const createBook(req, res) => res.send("is this working?") 
 
-// Register New User
 exports.register = async function(req, res) {
-    // req validation would be handled here
     const newUserInput = req.body
 
-    // TODO - Auth Service Register User
 
     res.json(newUserInput)
 }
