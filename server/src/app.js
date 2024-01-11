22e64aed1bbae98e039f371231f90c924e935918
William <robertw5@oregonstate.edu>
2024-01-11 10:31:04 -0500

    dynamoose app.js

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,8 @@ const morgan = require("morgan")
 const app = express()
 
 app.use(express.json())
+const dynamoose = require("dynamoose")
+dynamoose.aws.sdk.config.update({"region": "us-east-2"});
 app.use(morgan('dev'))
 app.use(require("./routes"))
 
