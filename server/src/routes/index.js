735d757653ad9a284b4592d062bc20d5129bc2c8
William <robertw5@oregonstate.edu>
2024-01-10 19:28:41 -0500

    index controller upd

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,1 +1,8 @@
-app.get('/', (req, res) => res.send('testing...'))
+const router = require('express').Router();
+const authRoutes = require('./Auth.routes')
+
+router.get('/', (req, res) => res.send('Hello world!'))
+
+router.use('/api/auth', authRoutes)
+
+module.exports = router;
