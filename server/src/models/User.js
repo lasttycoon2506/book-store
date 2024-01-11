19328c1ce42492de93940968f88af9ed7bc93bb9
William <robertw5@oregonstate.edu>
2024-01-10 19:07:27 -0500

    user model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,20 @@
-//to do user...
+const dynamoose = require("dynamoose");
+
+const userSchema = new dynamoose.Schema({
+    "id": String, // UUIDv4 ID
+    "username": String,
+    "password": String,
+    "firstName": String,
+    "lastName": String,
+    "email": String,
+    "address": String,
+    "city": String,
+    "state": String,
+    "zip": String,
+}, {
+    "timestamps": true
+})
+
+const User = dynamoose.model("User", userSchema)
+
+module.exports = User
