4f771b6dd5864d66c647b9c144991b2830447297
William <robertw5@oregonstate.edu>
2024-01-12 17:46:39 -0500

    push test...

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ const app = express()
 
 const dotenv = require('dotenv');
 dotenv.config();
-const config = require('../env').get(process.env.NODE_ENV);
 
 const morgan = require("morgan")
 app.use(morgan('dev'))
