5a7c414bec6515acefed791022cd924a94dfd2f0
William <robertw5@oregonstate.edu>
2024-01-12 19:39:21 -0500

    upd app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,7 @@
 const express = require('express')
 const app = express()
 
-const dotenv = require('dotenv');
-dotenv.config();
-
-const morgan = require("morgan")
-app.use(morgan('dev'))
-
 app.use(express.json())
-
-const dynamoose = require("dynamoose")
-dynamoose.aws.sdk.config.update({
-    "accessKeyId": config.awsAccessKeyId,
-    "secretAccessKey": config.awsSecretKey,
-    "region": "us-east-2",
-});
 
 app.use(require("./routes"))
 
