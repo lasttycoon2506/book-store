8e1b9194a98030d2fb691ee99f0d50e1deaf7066
William <robertw5@oregonstate.edu>
2024-01-13 19:03:49 -0500

    api.js added

---
+++ server/api.js
@@ -0,0 +1,9 @@
+const db = require("./db");
+const {
+    GetItemCommand,
+    PutItemCommand,
+    DeleteItemCommand,
+    ScanCommand,
+    UpdateItemCommand,
+} = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
