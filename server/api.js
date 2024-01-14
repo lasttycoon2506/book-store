d25b20f753cd91f4496f71a51baf6d5b8f684e55
William <robertw5@oregonstate.edu>
2024-01-13 19:09:21 -0500

    getPost added

--- server/api.js
+++ server/api.js
@@ -1,4 +1,4 @@
-const db = require("./db");
+const db = require("./dbConnector");
 const {
     GetItemCommand,
     PutItemCommand,
@@ -7,3 +7,32 @@ const {
     UpdateItemCommand,
 } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
+
+const getPost = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ postId: event.pathParameters.postId }),
+        };
+        const { Item } = await db.send(new GetItemCommand(params));
+
+        console.log({ Item });
+        response.body = JSON.stringify({
+            message: "Successfully retrieved post.",
+            data: (Item) ? unmarshall(Item) : {},
+            rawData: Item,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to get post.",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
