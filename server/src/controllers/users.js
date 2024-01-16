5590389dc6f06405cd397b93c1236bb34a4a2327
William <robertw5@oregonstate.edu>
2024-01-15 19:48:21 -0500

    user -> customer

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -3,14 +3,14 @@ const authentication = require('../services/auth')
 
 exports.register = async function(req, res) {
     try {
-        const newUser = req.body
-        newUser = await authentication.registerUser(newUserInput)
+        const newCustomer = req.body
+        newCustomer = await authentication.registerUser(newUserInput)
     }
     catch (err) {
         res.status(500).json({ message: "An error occurred while registering." });
     }
 
     
-    res.json(newUser)
+    res.json(newCustomer)
 }
 
