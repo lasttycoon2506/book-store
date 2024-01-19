08b7d545a597ef0a9a2101bbefbac65e2ce36b59
William <robertw5@oregonstate.edu>
2024-01-19 17:59:13 -0500

    getAllBooks book controller upd

--- server/src/controllers/books.js
+++ server/src/controllers/books.js
@@ -3,7 +3,8 @@ const {
     GetItemCommand,
     PutItemCommand,
     UpdateItemCommand, 
-    DeleteItemCommand
+    DeleteItemCommand,
+    ScanCommand
 } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
@@ -130,4 +131,28 @@ const deleteBook = async (event) => {
     return response;
 };
 
-module.exports = {getBook, createBook, updateBook, deleteBook}
+const getAllBooks = async () => {
+    const response = { statusCode: 200 };
+
+    try {
+        const { Items } = await db.send(new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME }));
+
+        response.body = JSON.stringify({
+            message: "Successfully retrieved all books.",
+            data: Items.map((item) => unmarshall(item)),
+            Items,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to retrieve books.",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
+
+module.exports = {getBook, createBook, updateBook, deleteBook, getAllBooks}
