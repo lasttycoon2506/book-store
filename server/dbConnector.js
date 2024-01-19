fd790354229ba3278080f6306cad565a37f728bb
William <robertw5@oregonstate.edu>
2024-01-19 15:22:54 -0500

    dynamodb local

--- server/dbConnector.js
+++ server/dbConnector.js
@@ -1,4 +1,9 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({
+    region: 'localhost',
+  endpoint: 'http://0.0.0.0:8000',
+  accessKeyId: 'MockAccessKeyId',
+  secretAccessKey: 'MockSecretAccessKey',
+});
 
 module.exports = client;
