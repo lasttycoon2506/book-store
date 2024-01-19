bc0ac62a5293b087014bd8dcb148840f3a60a17d
William <robertw5@oregonstate.edu>
2024-01-19 17:13:53 -0500

    serverless.yml migrate tables from aws

--- server/dbConnector.js
+++ server/dbConnector.js
@@ -1,6 +1,6 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const client = new DynamoDBClient({
-    region: 'localhost',
+  region: 'us-east-1',
   endpoint: 'http://0.0.0.0:8000',
   accessKeyId: 'MockAccessKeyId',
   secretAccessKey: 'MockSecretAccessKey',
