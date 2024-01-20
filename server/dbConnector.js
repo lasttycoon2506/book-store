a3a4af126e7b0b73a2ea06f9649ad41aa7079e39
William <robertw5@oregonstate.edu>
2024-01-20 15:47:42 -0500

    local server - aws console not loading

--- server/dbConnector.js
+++ server/dbConnector.js
@@ -1,11 +1,11 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const client = new DynamoDBClient({
-  
+  region: 'us-east-1',
+  endpoint: 'http://0.0.0.0:8000',
+  accessKeyId: 'MockAccessKeyId',
+  secretAccessKey: 'MockSecretAccessKey',
 });
 
-// region: 'us-east-1',
-  // endpoint: 'http://0.0.0.0:8000',
-  // accessKeyId: 'MockAccessKeyId',
-  // secretAccessKey: 'MockSecretAccessKey',
+
 
 module.exports = client;
