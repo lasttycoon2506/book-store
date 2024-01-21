52f4dd0acf4b347a5405109f9a585ab07847fb80
William <robertw5@oregonstate.edu>
2024-01-21 12:05:26 -0500

    Book entity upd

--- server/src/entities/Book.ts
+++ server/src/entities/Book.ts
@@ -1,4 +1,6 @@
-abstract class Book {
+import { Item } from "./AWSItem";
+
+export class Book extends Item {
   title: string;
   author: string;
   pages: number;
@@ -7,11 +9,22 @@ abstract class Book {
   stock: number;
   
   constructor(title, author, pages, genre, price, stock) {
+    super();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.genre = genre;
     this.price = price;
     this.stock = stock;
+  }
+
+  get pk(): string {
+    throw new Error("Method not implemented.");
+  }
+  get sk(): string {
+    throw new Error("Method not implemented.");
+  }
+  toItem(): Record<string, unknown> {
+    throw new Error("Method not implemented.");
   }
 }
