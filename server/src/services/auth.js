26792a6c0ca454a85eddcfcdd920a6ca79558cf0
William <robertw5@oregonstate.edu>
2024-02-11 19:10:55 -0500

    auth.js updated

--- server/src/services/auth.js
+++ server/src/services/auth.js
@@ -1,25 +1,12 @@
 import bcrypt from "bcryptjs"
 import {v4 as uuidv4} from "uuid"
 
-exports.registerCustomer = async function(newCustomer) {
+export const pwGenerator = async (event) => {
     try {
         const uuid = uuidv4()
         const salt = await bcrypt.genSalt(10)
         const hashedPass = await bcrypt.hash(newCustomer.password, salt)
 
-        //to-do customer model gen...
-        registeredCustomer = await Customer.create({
-            "id": uuid,
-            "username": newCustomer.Customername,
-            "password": hashedPass,
-            "firstName": newCustomer.firstName,
-            "lastName": newCustomer.lastName,
-            "email": newCustomer.email,
-            "address": newCustomer.address,
-            "city": newCustomer.city,
-            "state": newCustomer.state,
-            "zip": newCustomer.zip,
-        })
     } catch (err) {
         console.log(err)
         throw new Error(err)
