e35b32a4d845032d4880832c77c144df29303762
William <robertw5@oregonstate.edu>
2024-02-18 18:34:15 -0500

    book.ts constructor

--- server/src/entities/Book.ts
+++ server/src/entities/Book.ts
@@ -9,5 +9,16 @@ export class Book extends Item {
   genre: string;
   price: number;
   stock: number;
+
+  constructor(bookId: number, title: string, author: string, pages: number, genre: string, price: number, stock: number) {
+    super();
+    this.bookId = bookId;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.genre = genre;
+    this.price = price;
+    this.stock = stock;
+  }
 }
 
