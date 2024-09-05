e3fdc77a8c5c25126a4f982aae841d7710d9e6c9
William <robertw5@oregonstate.edu>
2024-09-05 15:40:51 -0400

    db.ts trimmed

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-
 export const client = new DynamoDBClient({
   region: 'us-east-1',
-  endpoint: 'http://0.0.0.0:8000',
-  accessKeyId: 'MockAccessKeyId',
-  secretAccessKey: 'MockSecretAccessKey',
+  endpoint: 'http://0.0.0.0:8000'
 });
 
