0e62c9c42ce7e395e029de867d6c98e58b71d34e
William <robertw5@oregonstate.edu>
2024-09-11 15:42:28 -0400

    data-stack upd.

--- server/src/stacks/data.ts
+++ server/src/stacks/data.ts
@@ -1,1 +1,12 @@
-//data-stack 
+import { Stack } from "aws-cdk-lib";
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { IBucket } from "aws-cdk-lib/aws-s3";
+
+export class data extends Stack {
+    public readonly booksTable: ITable;
+    public readonly booksBucket: IBucket;
+    
+    constructor(scope: , id: string, props?: ) {
+        // constructor implementation goes here
+    }
+}
