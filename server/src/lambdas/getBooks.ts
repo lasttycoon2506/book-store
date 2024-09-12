4d75a88e294e4e5b4518727bb2e5b04fa3f345d8
William <robertw5@oregonstate.edu>
2024-09-12 17:10:05 -0400

    getBooks typing

--- server/src/lambdas/getBooks.ts
+++ server/src/lambdas/getBooks.ts
@@ -1,5 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { APIGatewayProxyEvent } from "aws-lambda";
 
-export async function getBooks(event: , dbclient: DynamoDBClient) {
+
+export async function getBooks(event: APIGatewayProxyEvent, dbclient: DynamoDBClient) {
     
 }
