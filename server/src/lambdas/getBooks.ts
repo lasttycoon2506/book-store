8787f8a1d6f3a1c71cfc444cfe4dc0cb57d1025b
William <robertw5@oregonstate.edu>
2024-09-12 17:26:33 -0400

    books unmarshalled

--- server/src/lambdas/getBooks.ts
+++ server/src/lambdas/getBooks.ts
@@ -1,11 +1,12 @@
 import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 
 
 export async function getBooks(event: APIGatewayProxyEvent, dbclient: DynamoDBClient) {
     const allBooks = await dbclient.send(new ScanCommand({
         TableName: process.env.TABLE_NAME
-    }))
+    }));
         
-    
+    const allBooksUnmarshalled = allBooks.Items.map(item => unmarshall(item));
 }
