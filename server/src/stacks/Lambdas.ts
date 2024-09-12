f6312557b4909505e3f6f186ada7a808499b71cb
William <robertw5@oregonstate.edu>
2024-09-12 16:01:58 -0400

    custom-interface

--- server/src/stacks/Lambdas.ts
+++ server/src/stacks/Lambdas.ts
@@ -2,13 +2,17 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
 
+interface booksTableProps extends StackProps {
+    booksTable: ITable
+}
 
 export class Lambdas extends Stack {
 
     public readonly booksLambdaIntegration: LambdaIntegration;
     
-    constructor(scope: Construct, id: string , props: StackProps) {
+    constructor(scope: Construct, id: string , props: booksTableProps) {
         super(scope, id, props)
 
         const booksLambda = new NodejsFunction(this, 'BooksLambda', {
