079e81d1119bba7bd898c8a106f094a4038e2e15
William <robertw5@oregonstate.edu>
2024-09-12 15:54:35 -0400

    booksLambda obj

--- server/src/stacks/Lambdas.ts
+++ server/src/stacks/Lambdas.ts
@@ -1,10 +1,17 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 
 export class Lambdas extends Stack {
+
+    public readonly lambdas: NodejsFunction;
     
     constructor(scope: Construct, id: string , props: StackProps) {
         super(scope, id, props)
+
+        const booksLambda = new NodejsFunction(this, 'BooksLambda', {
+            
+        })
     }
 }
