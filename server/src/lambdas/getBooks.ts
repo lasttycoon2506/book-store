de0d4b1bc21532aa8c165733f27d9ccebf278369
William <robertw5@oregonstate.edu>
2024-09-12 17:19:24 -0400

    allBooks retrieved obj.

--- server/src/lambdas/getBooks.ts
+++ server/src/lambdas/getBooks.ts
@@ -1,7 +1,12 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent } from "aws-lambda";
 
 
 export async function getBooks(event: APIGatewayProxyEvent, dbclient: DynamoDBClient) {
+    const allBooks = dbclient.send(new ScanCommand({
+        TableName: process.env.TABLE_NAME
+    }
+        
     
+    ))
 }
