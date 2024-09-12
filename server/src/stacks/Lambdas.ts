47d856b72af8c8f5da28e3781302a775900c4e91
William <robertw5@oregonstate.edu>
2024-09-12 16:23:58 -0400

    books-lambda props

--- server/src/stacks/Lambdas.ts
+++ server/src/stacks/Lambdas.ts
@@ -1,8 +1,11 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { Runtime, Tracing } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+
 
 interface booksTableProps extends StackProps {
     booksTable: ITable
@@ -16,7 +19,13 @@ export class Lambdas extends Stack {
         super(scope, id, props)
 
         const booksLambda = new NodejsFunction(this, 'BooksLambda', {
-            
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'handler',
+            entry: (join(__dirname, '..','..', 'services', 'spaces', 'handler.ts')),
+            environment: {
+                TABLE_NAME: props.booksTable.tableName
+            },
+            timeout: Duration.minutes(1)
         })
     }
 }
