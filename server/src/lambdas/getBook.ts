3f0b8e4934fe71dd446006871602c3710f55205d
William <robertw5@oregonstate.edu>
2024-09-13 11:09:19 -0400

    lib-dynamodb library

--- server/src/lambdas/getBook.ts
+++ server/src/lambdas/getBook.ts
@@ -1,8 +1,16 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 export async function getBook(event: APIGatewayProxyEvent, dbclient: DynamoDBClient) {
     const bookId = event.body
 
+
+
     
+
+
+    dbclient.send(new GetItemCommand({
+
+    }))
 }
