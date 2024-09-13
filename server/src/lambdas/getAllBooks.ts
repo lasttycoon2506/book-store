d995f395ee7fb6b19743e682227caf247d3c0955
William <robertw5@oregonstate.edu>
2024-09-13 12:26:17 -0400

    err-handling getAllBooks

--- server/src/lambdas/getAllBooks.ts
+++ server/src/lambdas/getAllBooks.ts
@@ -3,10 +3,21 @@ import { unmarshall } from "@aws-sdk/util-dynamodb";
 
 
 export async function getAllBooks(dbclient: DynamoDBClient) {
+    try {
     const allBooks = await dbclient.send(new ScanCommand({
         TableName: process.env.TABLE_NAME
     }));
-        
+
     const allBooksUnmarshalled = allBooks.Items.map(item => unmarshall(item));
     return allBooksUnmarshalled
+
+    }
+    catch (error) {
+        console.log(error)
+        return {
+            statusCode: 400,
+            message: error.message
+        }
+    }    
+    
 }
