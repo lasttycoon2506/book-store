47042cb48dbaf143ae7bb95609d81adb2c5eeb93
William <robertw5@oregonstate.edu>
2024-09-13 14:02:13 -0400

    launch.json commments

--- server/src/test/launcher.ts
+++ server/src/test/launcher.ts
@@ -1,5 +1,6 @@
 import { handler } from "../lambdas/handler";
 
 process.env.AWS_REGION = "us-east-1";
+process.env.TABLE_NAME = "books-table";
 
 handler({httpMethod: 'GET'} as any, {} as any).then(result => console.log(result))
