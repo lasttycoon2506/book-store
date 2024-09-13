91576aad061e04fd1f3ff5b11bd9cc88aa4c30a0
William <robertw5@oregonstate.edu>
2024-09-13 12:54:12 -0400

    switch upd.

--- server/src/lambdas/handler.ts
+++ server/src/lambdas/handler.ts
@@ -1,14 +1,21 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { Context } from "vm";
+import { getBook } from "./getBook";
+import { getAllBooks } from "./getAllBooks";
 
 const dbClient = new DynamoDBClient({})
 
 async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
     switch (event.httpMethod) {
         case 'GET': 
-            const response = await getBooks(event, dbClient);
-            return response;
-    
+            if (event.queryStringParameters['id']) {
+                const response = await getBook(event, dbClient);
+                return response;
+            }
+            else {
+                const response = await getAllBooks(dbClient)
+                return response
+            }
     }
 }
