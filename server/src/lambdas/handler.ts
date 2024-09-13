509b36d305200901cbc13305bffe8a7bf586c743
William <robertw5@oregonstate.edu>
2024-09-13 12:33:12 -0400

    db-client obj

--- server/src/lambdas/handler.ts
+++ server/src/lambdas/handler.ts
@@ -2,14 +2,17 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { getBook } from "./getBook";
 import { getAllBooks } from "./getAllBooks";
+import { Context } from "vm";
 
-async function handler(event: APIGatewayProxyEvent, dbclient: DynamoDBClient) {
+const dbClient = new DynamoDBClient({})
+
+async function handler(event: APIGatewayProxyEvent, context: Context) {
     if (event.queryStringParameters['id']) {
-        const response = await getBook(event, dbclient)
+        const response = await getBook(event, dbClient)
         return response
     }
     else {
-        const response = await getAllBooks(dbclient)
+        const response = await getAllBooks(dbClient)
         return response
     }
 }
