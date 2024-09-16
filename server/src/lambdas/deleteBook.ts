d8c21af7d1196473c7447ac7dbc0541be2760227
William <robertw5@oregonstate.edu>
2024-09-16 10:58:24 -0400

    dynamodb-doc-client import

--- server/src/lambdas/deleteBook.ts
+++ server/src/lambdas/deleteBook.ts
@@ -1,6 +1,11 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { APIGatewayEvent } from "aws-lambda";
+import { DeleteItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export async function deleteBook(context: APIGatewayEvent, dbclient: DynamoDBClient): Promise<APIGatewayResult> {
+export async function deleteBook(context: APIGatewayEvent, dbclient: DynamoDBClient): Promise<APIGatewayProxyResult> {
+    const docClient = DynamoDBDocumentClient.from(dbclient);
 
+    const response = await dbclient.send(new DeleteItemCommand({
+        
+    }))
 }
