915aa22438e48587caa9098dc9e61717ca0208c8
William <robertw5@oregonstate.edu>
2024-09-16 11:39:19 -0400

    deleteBook param naming

--- server/src/lambdas/deleteBook.ts
+++ server/src/lambdas/deleteBook.ts
@@ -2,13 +2,13 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DeleteCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export async function deleteBook(context: APIGatewayEvent, dbclient: DynamoDBClient): Promise<APIGatewayProxyResult> {
+export async function deleteBook(event: APIGatewayEvent, dbclient: DynamoDBClient): Promise<APIGatewayProxyResult> {
     const docClient = DynamoDBDocumentClient.from(dbclient);
 
     const response = await docClient.send(new DeleteCommand({
         TableName: process.env.TABLE_NAME,
         Key: {
-            id: context.queryStringParameters["id"]
+            id: event.queryStringParameters["id"]
         }
     }));
 
--- server/src/lambdas/updateBook.ts
+++ server/src/lambdas/updateBook.ts
@@ -2,13 +2,24 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export async function updateBook(context: APIGatewayEvent, dbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
+export async function updateBook(event: APIGatewayEvent, dbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
     const docClient = DynamoDBDocumentClient.from(dbClient);
+    const parsedBody = JSON.parse(event.body);
 
     const response = docClient.send(new UpdateCommand({
         TableName: process.env.TABLE_NAME,
         Key: {
             id: context.queryStringParameters["id"]
-        }
+        },
+        UpdateExpression: 'set #zzzNew = :new',
+        ExpressionAttributeValues: {
+            ':new': {
+                S: requestBodyValue
+            }
+        },
+        ExpressionAttributeNames: {
+            '#zzzNew': requestBodyKey
+        },
+        ReturnValues: 'UPDATED_NEW'
     }))
 }
