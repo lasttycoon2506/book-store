298b317ca17ae91ac601ae0ab7f30d7cfb83139e
William <robertw5@oregonstate.edu>
2024-09-16 11:35:11 -0400

    update-response

--- server/src/lambdas/updateBook.ts
+++ server/src/lambdas/updateBook.ts
@@ -1,6 +1,14 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
 export async function updateBook(context: APIGatewayEvent, dbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
-    
+    const docClient = DynamoDBDocumentClient.from(dbClient);
+
+    const response = docClient.send(new UpdateCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: {
+            id: context.queryStringParameters["id"]
+        }
+    }))
 }
