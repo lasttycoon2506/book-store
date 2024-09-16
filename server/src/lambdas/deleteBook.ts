c07d743cde57307f72e646b212932537ac21898f
William <robertw5@oregonstate.edu>
2024-09-16 11:00:52 -0400

    delete from db

--- server/src/lambdas/deleteBook.ts
+++ server/src/lambdas/deleteBook.ts
@@ -1,11 +1,14 @@
 import { DeleteItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DeleteCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
 export async function deleteBook(context: APIGatewayEvent, dbclient: DynamoDBClient): Promise<APIGatewayProxyResult> {
     const docClient = DynamoDBDocumentClient.from(dbclient);
 
-    const response = await dbclient.send(new DeleteItemCommand({
-        
+    const response = await docClient.send(new DeleteCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: {
+            id: context.queryStringParameters["id"]
+        }
     }))
 }
