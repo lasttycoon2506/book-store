48f7331c2dd0ed118bba24e052e02897d5efb706
William <robertw5@oregonstate.edu>
2024-09-16 16:43:51 -0400

    update multi-items

--- server/src/lambdas/updateBook.ts
+++ server/src/lambdas/updateBook.ts
@@ -2,6 +2,7 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
+
 export async function updateBook(event: APIGatewayEvent, dbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
     const docClient = DynamoDBDocumentClient.from(dbClient);
     const parsedBody = JSON.parse(event.body);
@@ -10,19 +11,23 @@ export async function updateBook(event: APIGatewayEvent, dbClient: DynamoDBClien
 
 
     const response = await docClient.send(new UpdateCommand({
-        TableName: process.env.TABLE_NAME,
         Key: {
             id: event.queryStringParameters["id"]
         },
-        UpdateExpression: 'set #zzzNew = :new',
         ExpressionAttributeNames: {
-            '#zzzNew': requestBodyKey
+            "#T": requestBodyKey,
+            "#P": requestBodyKey
         },
         ExpressionAttributeValues: {
-            ':new': {
+            ':t': {
                 S: requestBodyValue
+            },
+            ':p': {
+                N: requestBodyValue
             }
         },
+        TableName: process.env.TABLE_NAME,
+        UpdateExpression: 'SET #T = :t, #P = :p',
         ReturnValues: 'ALL_NEW'
     }))
     return {
