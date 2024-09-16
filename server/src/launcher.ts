d316a433df2aa48d795623c58ff00a04e0263610
William <robertw5@oregonstate.edu>
2024-09-16 17:43:15 -0400

    api-stack init

--- server/src/launcher.ts
+++ server/src/launcher.ts
@@ -1,10 +1,14 @@
 import { App } from "aws-cdk-lib";
 import { Data } from "./stacks/Data";
 import { Lambdas } from "./stacks/Lambdas";
+import { Api } from "./stacks/Api";
 
 
-const app = new App()
+const app = new App();
 const dataStack = new Data(app, 'DataStack');
 const lambdaStack = new Lambdas(app, 'LambdaStack', {
     booksTable: dataStack.booksTable
-})
+});
+const apiStack = new Api(app, 'ApiStack', {
+    booksLambdaIntegration: lambdaStack.booksLambdaIntegration
+});
