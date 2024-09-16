0ee40c67a3887535f752b0ab66e20b9b64ba13c7
William <robertw5@oregonstate.edu>
2024-09-16 17:16:10 -0400

    api-class constructor

--- server/src/stacks/Api.ts
+++ server/src/stacks/Api.ts
@@ -1,10 +1,16 @@
 import { Stack, StackProps } from "aws-cdk-lib"
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway"
+import { Construct } from "constructs";
 
 interface ApiStackProps extends StackProps {
-    booksLambdaIntegration: LambdaIntegration
+    booksLambdaIntegration: LambdaIntegration;
 }
 
 export class Api extends Stack {
+    constructor(scope: Construct, id: string, props?: ApiStackProps) {
+        super(scope, id, props);
 
+
+        
+    }
 }
