d978e249d360aa228b7bd95486f9bf722f6d42b1
William <robertw5@oregonstate.edu>
2024-09-17 12:17:11 -0400

    launcher authstack

--- server/src/launcher.ts
+++ server/src/launcher.ts
@@ -2,6 +2,7 @@ import { App } from "aws-cdk-lib";
 import { Data } from "./stacks/Data";
 import { Lambdas } from "./stacks/Lambdas";
 import { Api } from "./stacks/Api";
+import { Authentication } from "./stacks/Authentication";
 
 
 const app = new App();
@@ -9,6 +10,7 @@ const dataStack = new Data(app, 'DataStack');
 const lambdaStack = new Lambdas(app, 'LambdaStack', {
     booksTable: dataStack.booksTable
 });
+const authenticationStack = new Authentication(app, 'AuthenticationStack');
 const apiStack = new Api(app, 'ApiStack', {
     booksLambdaIntegration: lambdaStack.booksLambdaIntegration
 });
