04db3d9c48b9269593be9bd0ff86470062eb8586
William <robertw5@oregonstate.edu>
2024-09-17 13:42:25 -0400

    auth init

--- server/src/stacks/Api.ts
+++ server/src/stacks/Api.ts
@@ -1,17 +1,25 @@
 import { Stack, StackProps } from "aws-cdk-lib"
-import { Cors, LambdaIntegration, ResourceOptions, RestApi } from "aws-cdk-lib/aws-apigateway"
+import { CognitoUserPoolsAuthorizer, Cors, LambdaIntegration, ResourceOptions, RestApi } from "aws-cdk-lib/aws-apigateway"
+import { IUserPool } from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
 
 interface ApiStackProps extends StackProps {
     booksLambdaIntegration: LambdaIntegration;
+    userPool: IUserPool;
 }
 
 export class Api extends Stack {
     constructor(scope: Construct, id: string, props?: ApiStackProps) {
         super(scope, id, props);
 
-
         const api = new RestApi(this, 'booksApi');
+
+        const authorizer = new CognitoUserPoolsAuthorizer(this, 'BookstoreApiAuthorizer', {
+            cognitoUserPools: [props.userPool],
+            identitySource: 'method.request.header.Authorization'
+        });
+
+        authorizer._attachToApi(api);
 
         const optionsWithCors: ResourceOptions = {
             defaultCorsPreflightOptions: {
