345324c4a163f95ae407c180752b655599760c8e
William <robertw5@oregonstate.edu>
2024-09-17 11:57:18 -0400

    auth-class variables init

--- server/src/stacks/Authentication.ts
+++ server/src/stacks/Authentication.ts
@@ -1,9 +1,19 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { UserPool, UserPoolClient } from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
 
 export class Authentication extends Stack{
+    private userPool: UserPool;
+    private userPoolClient: UserPoolClient;
+    
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        this.authenticationUserPool();
+        this.authnenticationUserPoolClient();
     }
+
+    private authenticationUserPool(){};
+    private authnenticationUserPoolClient(){};
 
 }
