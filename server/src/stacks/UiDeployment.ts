1daaedb39a6478a322963fe8ca0f19596835bf8f
William <robertw5@oregonstate.edu>
2024-09-18 12:05:50 -0400

    BucketDeployment

--- server/src/stacks/UiDeployment.ts
+++ server/src/stacks/UiDeployment.ts
@@ -1,6 +1,8 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
+import { existsSync } from "fs";
 import { join } from "path";
 
 export class UiDeployment extends Stack {
@@ -9,8 +11,18 @@ export class UiDeployment extends Stack {
 
         const deploymentBucket = new Bucket(this, 'UiDeploymentBucket', {
             bucketName: 'bookstore-frontend'
-        })
+        });
 
-        const uiPath = join(__dirname, "..", "..", "..", "client", "dist")
+        const uiPath = join(__dirname, "..", "..", "..", "client", "dist");
+        if (!existsSync(uiPath)) {
+            console.warn('ui path dne');
+        }
+
+        new BucketDeployment(this, 'BookstoreDeployment', {
+            sources: [Source.asset(uiPath)],
+            destinationBucket: deploymentBucket
+        });
+
+        
     }
 }
