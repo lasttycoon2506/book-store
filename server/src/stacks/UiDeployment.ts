105d122fb5d713cc5eace43d9448bee99897ae34
William <robertw5@oregonstate.edu>
2024-09-18 11:31:17 -0400

    deploymentBucket

--- server/src/stacks/UiDeployment.ts
+++ server/src/stacks/UiDeployment.ts
@@ -1,9 +1,14 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class UiDeployment extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const deploymentBucket = new Bucket(this, 'UiDeploymentBucket', {
+            bucketName: 'bookstore-frontend'
+        })
 
         
     }
