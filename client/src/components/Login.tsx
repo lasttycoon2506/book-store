985d92a0b418238b6af6b8a680f9b2c60ea78fa6
William <robertw5@oregonstate.edu>
2024-09-19 13:37:11 -0400

    login component

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,3 +1,14 @@
-export default function Login() {
-    
+import { useState } from "react";
+import { Authentication } from "../services/Authentication";
+
+type LoginProps = {
+    authentication: Authentication;
+    setUserNameCb: (userName: string) => void;
+}
+
+export default function Login({authentication, userNameCb}: LoginProps) {
+    const [userName, setUserName] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [errorMsg, setErrorMsg] = useState<string>("");
+    const [loginSuccess, setLoginSuccess] = useState<boolean>(false);
 }
