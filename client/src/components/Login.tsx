82a45365c3d7b948eef6bf26e6aee370f99358f7
William <robertw5@oregonstate.edu>
2024-09-19 13:51:22 -0400

    submit func.

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 import { Authentication } from "../services/Authentication";
 
 type LoginProps = {
@@ -6,9 +6,29 @@ type LoginProps = {
     setUserNameCb: (userName: string) => void;
 }
 
-export default function Login({authentication, userNameCb}: LoginProps) {
+export default function Login({authentication, setUserNameCb}: LoginProps) {
     const [userName, setUserName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [errorMsg, setErrorMsg] = useState<string>("");
     const [loginSuccess, setLoginSuccess] = useState<boolean>(false);
+
+    const submit = async (event: SyntheticEvent) => {
+        event.preventDefault();
+        if (userName && password) {
+            const loginResult = await authentication.login(userName, password);
+            const userName2 = authentication.getUserName();
+            if (userName2) {
+                setUserNameCb(userName2);
+            }
+            if (loginResult) {
+                setLoginSuccess(true);
+            }
+            else {
+                setErrorMsg('invalid creds.');
+            }
+        }
+        else {
+            setErrorMsg('username & pw required');
+        };
+    };
 }
