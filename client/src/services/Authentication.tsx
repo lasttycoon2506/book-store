5b0aa4ef6c6ba47de929cc25827a83d94bfd0fae
William <robertw5@oregonstate.edu>
2024-09-19 15:43:17 -0400

    amplify cognito client config

--- client/src/services/Authentication.tsx
+++ client/src/services/Authentication.tsx
@@ -1,6 +1,18 @@
+import { Amplify } from "aws-amplify";
+import { AuthenticationStack } from "../../../server/outputs.json"
+
 const awsRegion = "us-east-1";
 
-Amplify.configure
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: AuthenticationStack.BookstoreUserPoolId,
+            userPoolClientId: AuthenticationStack.BookstoreUserPoolClientId,
+            identityPoolId: AuthenticationStack.BookstoreIdentityPoolId
+        }
+    }
+})
+
 export class Authentication {
     async login(userName: string, password: string): Promise<Object | undefined> {
         return {
