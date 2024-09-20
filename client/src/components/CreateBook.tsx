55e0c616ec23e859867d05ef0a8184d08d083aeb
William <robertw5@oregonstate.edu>
2024-09-20 11:35:49 -0400

    createBook form

--- client/src/components/CreateBook.tsx
+++ client/src/components/CreateBook.tsx
@@ -10,7 +10,22 @@ export default function CreateBook({ database }: CreateBookProps) {
     const [title, setTitle] = useState<string>("");
     const [author, setAuthor] = useState<string>("");
 
-    if (!database.isAuthorized()) {
-        return <NavLink to="/login"> Login </NavLink>
+    function submit(){
+
     }
+
+    function renderForm() {
+        if (!database.isAuthorized()) {
+            return <NavLink to="/login"> Login </NavLink>
+        }
+        return (
+            <form onSubmit={(e) => submit(e)}>
+                <label> title </label>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} />
+                <label> author </label>
+                <input value={author} onChange={(e) => setAuthor(e.target.value)} />
+            </form>
+        )
+    }
+
 }
