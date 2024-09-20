f3c84a5dc231faf0669a89542c0e872715a7858d
William <robertw5@oregonstate.edu>
2024-09-20 11:25:59 -0400

    createBook unauth.

--- client/src/components/CreateBook.tsx
+++ client/src/components/CreateBook.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import { Database } from "../services/Database";
+import { NavLink } from "react-router-dom";
+
 type CreateBookProps = {
     database: Database;
 }
 
 export default function CreateBook({ database }: CreateBookProps) {
+    const [title, setTitle] = useState<string>("");
+    const [author, setAuthor] = useState<string>("");
 
+    if (!database.isAuthorized()) {
+        return <NavLink to="/login"> Login </NavLink>
+    }
 }
