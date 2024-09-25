7aff32ef3744318610465bfaa395ba0b62fc18f8
William <robertw5@oregonstate.edu>
2024-09-25 12:35:44 -0400

    getAllBooks() effect

--- client/src/components/ViewAllBooks.tsx
+++ client/src/components/ViewAllBooks.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Database } from "../services/Database"
 import { Book } from "../models/model";
 
@@ -6,8 +6,16 @@ type ViewAllBooksProps = {
     database: Database;
 }
 
-export default function ViewAllBooks(database: ViewAllBooksProps){
+export default function ViewAllBooks({database}: ViewAllBooksProps){
     const [books, setBooks] = useState<Book[]>();
+
+    useEffect(() => {
+        const getAllBooks = async () => { 
+            const allBooks = await database.getAllBooks();
+            setBooks(allBooks);
+        }
+        getAllBooks();
+    }, []);
 
     
 }
