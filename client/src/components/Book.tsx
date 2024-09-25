eedf38bd4f2d578a523d9b5a8e6eac854d36aeb3
William <robertw5@oregonstate.edu>
2024-09-25 13:33:59 -0400

    book-component removed newlines

--- client/src/components/Book.tsx
+++ client/src/components/Book.tsx
@@ -4,15 +4,10 @@ export default function Book(props: Book): JSX.Element {
     return (
         <div className='bookComponent'>
             <label className='title'>{props.title}</label>
-            <br />
             <label className='author'>{props.author}</label>
-            <br />
             <label className='pages'>{props.pages}</label>
-            <br />
             <label className='genre'>{props.genre}</label>
-            <br />
             <label className='price'>{props.price}</label>
-            <br />
             <label className='stock'>{props.stock}</label>
         </div>
     );
