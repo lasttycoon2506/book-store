b697fa4c3b498d7b36c1d561145c39f1601dd6c7
William <robertw5@oregonstate.edu>
2024-09-27 11:25:54 -0400

    updatedTitleSetter()

--- client/src/components/ViewAllBooks.tsx
+++ client/src/components/ViewAllBooks.tsx
@@ -3,8 +3,8 @@ import { Database } from "../services/Database"
 import type { Book as BookModel} from "../models/model";
 import { NavLink, useNavigate } from "react-router-dom";
 import type {} from '@mui/x-data-grid/themeAugmentation';
-import { GridRowsProp } from "@mui/x-data-grid/models/gridRows";
-import { GridColDef } from "@mui/x-data-grid/models/colDef/gridColDef";
+import { GridRowsProp, GridValidRowModel } from "@mui/x-data-grid/models/gridRows";
+import { GridColDef, GridValueSetter } from "@mui/x-data-grid/models/colDef/gridColDef";
 import { DataGrid } from "@mui/x-data-grid/DataGrid/DataGrid";
 
 
@@ -35,18 +35,23 @@ export default function ViewAllBooks({database}: ViewAllBooksProps){
         getAllBooks();
     }, []);
       
-      const rows: GridRowsProp = 
-       books!
-      ;
-      
-      const columns: GridColDef[] = [
-        { field: 'title', headerName: 'Title', width: 150, editable: true},
-        { field: 'author', headerName: 'Author', width: 150, editable: true},
-        { field: 'pages', headerName: 'Pages', width: 150, editable: true},
-        { field: 'genre', headerName: 'Genre', width: 150, editable: true},
-        { field: 'price', headerName: 'Price', width: 150, editable: true},
-        { field: 'stock', headerName: 'Stock', width: 150, editable: true}
-      ];
+    const setUpdatedTitle: GridValueSetter<GridValidRowModel> = (value, row) => {
+      const [title] = value;
+      return { ...row, title };
+    };
+
+    const rows: GridRowsProp = 
+      books!
+    ;
+
+    const columns: GridColDef[] = [
+      { field: 'title', headerName: 'Title', width: 150, editable: true, valueSetter: setUpdatedTitle},
+      { field: 'author', headerName: 'Author', width: 150, editable: true},
+      { field: 'pages', headerName: 'Pages', width: 150, editable: true},
+      { field: 'genre', headerName: 'Genre', width: 150, editable: true},
+      { field: 'price', headerName: 'Price', width: 150, editable: true},
+      { field: 'stock', headerName: 'Stock', width: 150, editable: true},
+    ];
       
 
     function loadAllBooks() {
@@ -55,7 +60,14 @@ export default function ViewAllBooks({database}: ViewAllBooksProps){
         }
         return (
           <div style={{ height: 300, width: '100%' }}>
-        <DataGrid rows={rows} columns={columns} />
+        <DataGrid 
+          rows={rows} 
+          columns={columns}
+          processRowUpdate={(updatedRow, originalRow) =>
+            mySaveOnServerFunction(updatedRow);
+          }
+          onProcessRowUpdateError={handleProcessRowUpdateError} 
+          />
         </div>
         )
     };
