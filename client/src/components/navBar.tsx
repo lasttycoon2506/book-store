e83b9bc2189fd919b55a0a1a65229b783c7da8dd
William <robertw5@oregonstate.edu>
2024-10-01 13:52:54 -0400

    appbar skele

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -1,4 +1,12 @@
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
 import { NavLink } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
 
 
 type NavBarProps = {
@@ -6,29 +14,66 @@ type NavBarProps = {
 };
 
 export default function NavBar({ userName } : NavBarProps): JSX.Element {
-    function renderLoginLogout(): JSX.Element {
-        if (userName) {
-            return (
-                <NavLink to ="/logout" style={{float:"right"}}> 
-                    {userName} 
-                </NavLink> 
-            );
-        }
-        else {
-            return (
-                <NavLink to = "/login" style={{float:"right"}}>
-                    Login
-                </NavLink>
-            );
-        }
-    }
+    // const [auth, setAuth] = useState(true);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setAuth(event.target.checked);
+    // };
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <div className="navbar">
-            <NavLink to="/"> Home </NavLink>
-            <NavLink to="/profile"> Profile </NavLink>
-            <NavLink to="/books"> Books </NavLink>
-            <NavLink to="/createBook"> Create Book</NavLink>
-            {renderLoginLogout()}
-        </div>
+        <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {userName && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
     )
 }
