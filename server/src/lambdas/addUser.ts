f0374f38f46c57fca86ca7250778bbe80ea82858
William <robertw5@oregonstate.edu>
2024-10-07 15:12:25 -0400

    client-cognito pckg

--- server/src/lambdas/addUser.ts
+++ server/src/lambdas/addUser.ts
@@ -1,8 +1,15 @@
+import { CognitoIdentityProviderClient, AdminCreateUserCommand } from "@aws-sdk/client-cognito-identity-provider";
+
+
 type User = {
     userName: string;
     passWord: string;
 }
 
+const client = new CognitoIdentityProviderClient(config);
+
 export function addUser(user: User): Promise<unknown> {
     return
 }
+
+
