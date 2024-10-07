752f8aaa871aa04167f21bb2033f48439cabfe8e
William <robertw5@oregonstate.edu>
2024-10-07 15:19:40 -0400

    client config

--- server/src/lambdas/addUser.ts
+++ server/src/lambdas/addUser.ts
@@ -6,6 +6,10 @@ type User = {
     passWord: string;
 }
 
+const config = {
+    region: 'us-east-1'
+}
+
 const client = new CognitoIdentityProviderClient(config);
 
 export function addUser(user: User): Promise<unknown> {
