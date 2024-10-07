0fddc8d2a97cc2a6937634dcceb5d9b489043039
William <robertw5@oregonstate.edu>
2024-10-07 15:39:12 -0400

    formatting

--- server/src/lambdas/addUser.ts
+++ server/src/lambdas/addUser.ts
@@ -1,37 +1,41 @@
-import { CognitoIdentityProviderClient, AdminCreateUserCommand, AdminCreateUserCommandOutput } from "@aws-sdk/client-cognito-identity-provider";
-import { AuthenticationStack } from "../../outputs.json";
+import {
+    CognitoIdentityProviderClient,
+    AdminCreateUserCommand,
+    AdminCreateUserCommandOutput,
+} from '@aws-sdk/client-cognito-identity-provider'
+import { AuthenticationStack } from '../../outputs.json'
 
 type User = {
-    userName: string;
-    passWord: string;
+    userName: string
+    passWord: string
 }
 
 const config = {
-    region: "us-east-1"
+    region: 'us-east-1',
 }
 
-const client = new CognitoIdentityProviderClient(config);
+const client = new CognitoIdentityProviderClient(config)
 
-export async function addUser(user: User): Promise<AdminCreateUserCommandOutput> {
-    const input = { 
-        UserPoolId: `${AuthenticationStack.BookstoreUserPoolId}`, 
-        Username: `${user.userName}`, 
-        UserAttributes: [ 
-          { 
-            Name: "STRING_VALUE", 
-            Value: "STRING_VALUE",
-          },
+export async function addUser(
+    user: User
+): Promise<AdminCreateUserCommandOutput> {
+    const input = {
+        UserPoolId: `${AuthenticationStack.BookstoreUserPoolId}`,
+        Username: `${user.userName}`,
+        UserAttributes: [
+            {
+                Name: 'STRING_VALUE',
+                Value: 'STRING_VALUE',
+            },
         ],
         ValidationData: [
-          {
-            Name: "STRING_VALUE", 
-            Value: "STRING_VALUE",
-          },
-        ]
-      };
-      const command = new AdminCreateUserCommand(input);
-const response = await client.send(command);
-return response;
+            {
+                Name: 'STRING_VALUE',
+                Value: 'STRING_VALUE',
+            },
+        ],
+    }
+    const command = new AdminCreateUserCommand(input)
+    const response = await client.send(command)
+    return response
 }
-
-
