1aad78bf01386d4116fca9b29ae8bdccb4dcfb3c
William <robertw5@oregonstate.edu>
2024-10-10 15:16:32 -0400

    display fe profile skele

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,9 +1,45 @@
+import { useState } from "react"
 import { User } from "../models/User"
+import Box from "@mui/material/Box"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import PhoneIcon from '@mui/icons-material/Phone';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import ListItemText from "@mui/material/ListItemText"
 
 type profileProps = {
     userProfile: User
 }
 
 export default function Profile(userProfile: profileProps) {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [phone, setPhone] = useState()
 
+    return (
+        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          <nav aria-label="main mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <PhoneIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Inbox" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <DraftsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Drafts" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Box>
+      );
 }
