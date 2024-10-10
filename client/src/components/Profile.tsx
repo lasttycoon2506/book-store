f745406c7bc5f2e6c1d2d1d7c540a2f4762e862e
William <robertw5@oregonstate.edu>
2024-10-10 15:22:01 -0400

    profile comp. formatting

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react"
-import { User } from "../models/User"
-import Box from "@mui/material/Box"
-import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemIcon from "@mui/material/ListItemIcon"
-import PhoneIcon from '@mui/icons-material/Phone';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import ListItemText from "@mui/material/ListItemText"
+import { useState } from 'react'
+import { User } from '../models/User'
+import Box from '@mui/material/Box'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import PhoneIcon from '@mui/icons-material/Phone'
+import DraftsIcon from '@mui/icons-material/Drafts'
+import ListItemText from '@mui/material/ListItemText'
 
 type profileProps = {
     userProfile: User
@@ -20,26 +20,26 @@ export default function Profile(userProfile: profileProps) {
 
     return (
         <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          <nav aria-label="main mailbox folders">
-            <List>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <PhoneIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Inbox" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <DraftsIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Drafts" />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </nav>
+            <nav aria-label="main mailbox folders">
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <PhoneIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Inbox" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <DraftsIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Drafts" />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </nav>
         </Box>
-      );
+    )
 }
