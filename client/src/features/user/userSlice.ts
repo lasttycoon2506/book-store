33bc770a8ec0540027e2932b2e8249051cfb8e63
William <robertw5@oregonstate.edu>
2024-10-11 14:13:32 -0400

    redux save-reducer

--- client/src/features/user/userSlice.ts
+++ client/src/features/user/userSlice.ts
@@ -16,20 +16,13 @@ export const userProfileSlice = createSlice({
   name: 'userProfile',
   initialState,
   reducers: {
-    save: (state) => {
-      state.value 
-    },
-    decrement: (state) => {
-      state.value 
-    },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      // state.value += action.payload
+    save: (state, action: PayloadAction<UserProfile>) => {
+      state.value = action.payload
     },
   },
 })
 
-export const { increment, decrement, incrementByAmount } = userProfileSlice.actions
+export const { save } = userProfileSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectUser = (state: RootState) => state.userProfile.value
