df4bf83ae42d39950950aecf0ba71ac385705f35
William <robertw5@oregonstate.edu>
2024-10-14 15:21:07 -0400

    removed unused dep./props

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -2,24 +2,14 @@ import { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import PhoneIcon from '@mui/icons-material/Phone'
 import DraftsIcon from '@mui/icons-material/Drafts'
 import FaceIcon from '@mui/icons-material/Face'
 import ListItemText from '@mui/material/ListItemText'
-import { Authentication } from '../services/Authentication'
-import { useAppSelector } from '../app/hooks'
-import { selectUserProfile } from '../features/user/userSlice'
-import Login from './Login'
-
-type profileProps = {
-    authentication: Authentication
-}
-
 import { fetchUserAttributes } from 'aws-amplify/auth'
 
-export default function Profile({ authentication }: profileProps) {
+export default function Profile() {
     const [name, setName] = useState<string>()
     const [email, setEmail] = useState<string>()
     const [phone, setPhone] = useState<string>()
