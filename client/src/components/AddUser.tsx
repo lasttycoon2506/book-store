7246a9ce579efd565853c5e2c2d64c63bf1d2187
William Roberts <werobert@yahoo.com>
2024-10-15 13:25:40 -0400

    addUser() skele

---
+++ client/src/components/AddUser.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+
+export default function AddUser() {
+    const [userName, setUserName] = useState<string>()
+    const [password, setpassword] = useState<string>()
+}
