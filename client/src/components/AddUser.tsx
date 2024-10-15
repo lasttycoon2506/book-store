f142b1fa666813e0d30a3f73b09259dc3f1dc6d1
William Roberts <werobert@yahoo.com>
2024-10-15 13:28:02 -0400

    AddUser() return html

--- client/src/components/AddUser.tsx
+++ client/src/components/AddUser.tsx
@@ -1,6 +1,41 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
 
 export default function AddUser() {
     const [userName, setUserName] = useState<string>()
-    const [password, setpassword] = useState<string>()
+    const [password, setPassword] = useState<string>()
+
+    return (
+        <div role="main">
+            <Box
+                component="form"
+                sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+                noValidate
+                autoComplete="off"
+                onSubmit={(e) => submit(e)}
+            >
+                <TextField
+                    value={userName}
+                    label="User Name"
+                    variant="outlined"
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <br />
+                <TextField
+                    value={password}
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <Button variant="contained" size="large" type="submit">
+                    Login
+                </Button>
+            </Box>
+            <br />
+        </div>
+    )
 }
