6280e9e3caac8b8e868c5748bd2334c44bcf2f56
William Roberts <werobert@yahoo.com>
2024-10-15 15:08:10 -0400

    formating

--- client/src/components/AddUser.tsx
+++ client/src/components/AddUser.tsx
@@ -25,19 +25,18 @@ export default function AddUser({ database }: AddUserProps) {
 
     async function submit(event: SyntheticEvent): Promise<void> {
         if (userName && password && name && email && phone) {
-                const user: User = {
-                    userName: userName,
-                    password: password,
-                    name: name,
-                    email: email,
-                    phone: phone,
-                }
-                const addUserResponse = await database.addUser(user)
-                if (addUserResponse === 201) {
-                    setAlert(true)
-                    resetFields()
-                }
-
+            const user: User = {
+                userName: userName,
+                password: password,
+                name: name,
+                email: email,
+                phone: phone,
+            }
+            const addUserResponse = await database.addUser(user)
+            if (addUserResponse === 201) {
+                setAlert(true)
+                resetFields()
+            }
         }
     }
 
@@ -47,101 +46,96 @@ export default function AddUser({ database }: AddUserProps) {
         setName('')
         setEmail('')
         setPhone('')
-        
     }
     function renderForm(): JSX.Element {
-    if (!database.isAuthorized()) {
+        if (!database.isAuthorized()) {
+            return (
+                <>
+                    <br />
+                    <NavLink to={'/login'}> Must Login First</NavLink>
+                </>
+            )
+        }
         return (
-            <>
+            <div role="main">
+                <Box
+                    component="form"
+                    sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={(e) => submit(e)}
+                >
+                    <div>
+                        {alert ? (
+                            <Collapse in={alertOpen}>
+                                {' '}
+                                <Alert
+                                    action={
+                                        <IconButton
+                                            aria-label="close"
+                                            color="inherit"
+                                            size="small"
+                                            onClick={() => {
+                                                setAlertOpen(false)
+                                            }}
+                                        >
+                                            <CloseIcon fontSize="inherit" />
+                                        </IconButton>
+                                    }
+                                >
+                                    User Created!
+                                </Alert>
+                            </Collapse>
+                        ) : (
+                            <></>
+                        )}
+                    </div>
+                    <TextField
+                        value={userName}
+                        label="User Name"
+                        variant="outlined"
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    <br />
+                    <TextField
+                        value={password}
+                        label="Password"
+                        variant="outlined"
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <TextField
+                        value={name}
+                        label="Name"
+                        variant="outlined"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <TextField
+                        value={email}
+                        label="Email"
+                        variant="outlined"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <TextField
+                        value={phone}
+                        label="Phone"
+                        variant="outlined"
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                    <br />
+                    <Button variant="contained" size="large" type="submit">
+                        Create
+                    </Button>
+                </Box>
                 <br />
-                <NavLink to={'/login'}> Must Login First</NavLink>
-            </>
+            </div>
         )
     }
-    return (
-        <div role="main">
-            <Box
-                component="form"
-                sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-                noValidate
-                autoComplete="off"
-                onSubmit={(e) => submit(e)}
-            >
-                <div>
-                    {alert ? (
-                        <Collapse in={alertOpen}>
-                            {' '}
-                            <Alert
-                                action={
-                                    <IconButton
-                                        aria-label="close"
-                                        color="inherit"
-                                        size="small"
-                                        onClick={() => {
-                                            setAlertOpen(false)
-                                        }}
-                                    >
-                                        <CloseIcon fontSize="inherit" />
-                                    </IconButton>
-                                }
-                            >
-                                User Created!
-                            </Alert>
-                        </Collapse>
-                    ) : (
-                        <></>
-                    )}
-                </div>
-                <TextField
-                    value={userName}
-                    label="User Name"
-                    variant="outlined"
-                    onChange={(e) => setUserName(e.target.value)}
-                />
-                <br />
-                <TextField
-                    value={password}
-                    label="Password"
-                    variant="outlined"
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <br />
-                <br />
-                <TextField
-                    value={name}
-                    label="Name"
-                    variant="outlined"
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <br />
-                <br />
-                <TextField
-                    value={email}
-                    label="Email"
-                    variant="outlined"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <br />
-                <br />
-                <TextField
-                    value={phone}
-                    label="Phone"
-                    variant="outlined"
-                    onChange={(e) => setPhone(e.target.value)}
-                />
-                <br />
-                <Button variant="contained" size="large" type="submit">
-                    Create
-                </Button>
-            </Box>
-            <br />
-        </div>
-    )
-}
-return (
-    <>
-        {renderForm()}
-    </>
-)
+    return <>{renderForm()}</>
 }
