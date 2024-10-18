c5f0bf0c912a7c985dfb8b036d2fa50d9eb44d24
William Roberts <werobert@yahoo.com>
2024-10-18 16:06:03 -0400

    book id field added

--- server/src/models/Book.ts
+++ server/src/models/Book.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 
 export const Book = z.object({
+    id: z.string().uuid(),
     title: z.string().min(1),
     author: z.string().min(1),
     pages: z.number().int().nonnegative(),
