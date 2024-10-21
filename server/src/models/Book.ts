b5a9d3ef616e52763ab3d1ea7e80c95069b7708c
William Roberts <werobert@yahoo.com>
2024-10-21 16:01:51 -0400

    pgs > 0 req.

--- server/src/models/Book.ts
+++ server/src/models/Book.ts
@@ -4,7 +4,7 @@ export const Book = z.object({
     id: z.string().uuid().optional(),
     title: z.string().min(1),
     author: z.string().min(1),
-    pages: z.number().int().nonnegative(),
+    pages: z.number().int().positive(),
     genre: z.string().min(1),
     price: z.number().nonnegative(),
     stock: z.number().int().nonnegative(),
