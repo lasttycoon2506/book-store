3b159258c77f4703bf6f7cd76c2a5a4537e3bc58
William Roberts <werobert@yahoo.com>
2024-10-21 16:00:59 -0400

    num fields err msgs

--- client/src/zod/schemas/Book.ts
+++ client/src/zod/schemas/Book.ts
@@ -1,11 +1,30 @@
 import { z } from 'zod'
 
-export const BookSchema = z.object({
-    id: z.string().uuid().optional(),
-    title: z.string(),
-    author: z.string(),
-    pages: z.coerce.number().int().nonnegative(),
-    genre: z.string(),
-    price: z.coerce.number().nonnegative(),
-    stock: z.coerce.number().int().nonnegative(),
-}).required()
+export const BookSchema = z
+    .object({
+        id: z.string().uuid().optional(),
+        title: z.string(),
+        author: z.string(),
+        pages: z.coerce
+            .number({
+                required_error: 'Pages is required',
+                invalid_type_error: 'Pages must be an integer > 0',
+            })
+            .int()
+            .positive(),
+        genre: z.string(),
+        price: z.coerce
+            .number({
+                required_error: 'Price is required',
+                invalid_type_error: 'Price must be a number > 0',
+            })
+            .nonnegative(),
+        stock: z.coerce
+            .number({
+                required_error: 'Stock is required',
+                invalid_type_error: 'Stock must be an integer >= 0',
+            })
+            .int()
+            .nonnegative(),
+    })
+    .required()
