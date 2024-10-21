38849641155296b56150e0abb4de24369adec985
William Roberts <werobert@yahoo.com>
2024-10-21 15:50:04 -0400

    mui/materials reinstall

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
+    plugins: [react()],
+    define: {
+        // By default, Vite doesn't include shims for NodeJS/
+        // necessary for segment analytics lib to work
+        global: {},
     },
-  },
+    resolve: {
+        alias: {
+            './runtimeConfig': './runtimeConfig.browser',
+        },
+    },
 })
