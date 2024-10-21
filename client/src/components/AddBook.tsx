2520c75c27a69d038bc8de7dbe1a9d7af1aa9c3f
William Roberts <werobert@yahoo.com>
2024-10-21 17:03:31 -0400

    removed unused code

--- client/src/components/AddBook.tsx
+++ client/src/components/AddBook.tsx
@@ -16,120 +16,23 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { BookSchema } from '../zod/schemas/Book'
 import React from 'react'
 
-
 type AddBookProps = {
     database: Database
 }
 
 export default function AddBook({ database }: AddBookProps): JSX.Element {
-    // const [title, setTitle] = useState<string>('')
-    const [author, setAuthor] = useState<string>('')
-    const [pages, setPages] = useState<number>(0)
-    const [genre, setGenre] = useState<string>('')
-    const [price, setPrice] = useState<number>(0)
-    const [stock, setStock] = useState<number>(0)
     const [alert, setAlert] = useState<boolean>(false)
     const [alertOpen, setAlertOpen] = useState<boolean>(true)
-    // const [errorTitle, setErrorTitle] = useState<boolean>(false)
-    const [errorAuthor, setErrorAuthor] = useState<boolean>(false)
-    const [errorPages, setErrorPages] = useState<boolean>(false)
-    const [errorGenre, setErrorGenre] = useState<boolean>(false)
-    const [errorPrice, setErrorPrice] = useState<boolean>(false)
-    const [errorStock, setErrorStock] = useState<boolean>(false)
-    const { register, handleSubmit, formState: {errors} } = useForm({resolver: zodResolver(BookSchema)})
 
-   
-
-    // async function handleSubmits(event: SyntheticEvent): Promise<void> {
-    //     event.preventDefault()
-    //     isTitleEmpty(title)
-    //     isAuthorEmpty(author)
-    //     isPagesEmpty(pages!)
-    //     isGenreEmpty(genre)
-    //     isPriceEmpty(price!)
-    //     isStockEmpty(stock!)
-    //     if (title && author && pages && genre && price && stock) {
-    //         const book: Book = {
-    //             title: title,
-    //             author: author,
-    //             pages: pages,
-    //             genre: genre,
-    //             price: price,
-    //             stock: stock,
-    //         }
-    //         const addBookResponse = await database.addBook(book)
-    //         if (addBookResponse) {
-    //             setAlert(true)
-    //             resetFields()
-    //         } else {
-    //             console.error('Unable to create book!')
-    //         }
-    //     }
-    // }
-
-    // function isTitleEmpty(title: string): void {
-    //     if (!title) {
-    //         setErrorTitle(true)
-    //     } else {
-    //         setErrorTitle(false)
-    //     }
-    //     return
-    // }
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({ resolver: zodResolver(BookSchema) })
 
     function inputValidation(data: Book) {
         console.log(data)
     }
-    function isAuthorEmpty(author: string): void {
-        if (!author) {
-            setErrorAuthor(true)
-        } else {
-            setErrorAuthor(false)
-        }
-        return
-    }
-    function isPagesEmpty(pages: number): void {
-        if (!pages) {
-            setErrorPages(true)
-        } else {
-            setErrorPages(false)
-        }
-        return
-    }
-    function isGenreEmpty(genre: string): void {
-        if (!genre) {
-            setErrorGenre(true)
-        } else {
-            setErrorGenre(false)
-        }
-        return
-    }
-    function isPriceEmpty(price: number): void {
-        if (!price) {
-            setErrorPrice(true)
-        } else {
-            setErrorPrice(false)
-        }
-        return
-    }
-    function isStockEmpty(stock: number): void {
-        if (!stock) {
-            setErrorStock(true)
-        } else {
-            setErrorStock(false)
-        }
-        return
-    }
-
-    function resetFields(): void {
-        // setTitle('')
-        setAuthor('')
-        setPages(0)
-        setGenre('')
-        setPrice(0)
-        setStock(0)
-    }
-
-    
 
     function renderForm(): JSX.Element {
         if (!database.isAuthorized()) {
@@ -143,14 +46,10 @@ export default function AddBook({ database }: AddBookProps): JSX.Element {
         return (
             <Box
                 component="form"
-                // sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-                // noValidate
                 autoComplete="off"
                 onSubmit={handleSubmit((data: FieldValues) => {
                     inputValidation(data as Book)
-                    
-                })
-                }
+                })}
             >
                 <div>
                     {alert ? (
@@ -177,109 +76,66 @@ export default function AddBook({ database }: AddBookProps): JSX.Element {
                         <></>
                     )}
                 </div>
-                <TextField
-                    // value={title}
-                    // label="Title"
-                    // variant="outlined"
-                    // error={errorTitle}
-                    // onChange={(e) => setTitle(e.target.value)}
-                    {...register('title', {
-                        required: true,
-                        minLength: { value: 1, message: 'min. length 1' },
-                    })}
-                    placeholder="Title"
-                />
-                <p>{(errors['title']?.message) ? (String(errors['title']?.message)) : (
+                <TextField {...register('title')} placeholder="Title" />
+                <p>
+                    {errors['title']?.message ? (
+                        String(errors['title']?.message)
+                    ) : (
                         <></>
-                    )}</p>
-                <TextField
-                    // value={author}
-                    // label="Author"
-                    // variant="outlined"
-                    // error={errorAuthor}
-                    // onChange={(e) => setAuthor(e.target.value)}
-                    {...register('author', {
-                        required: 'Author required',
-                        minLength: { value: 1, message: 'min. length 1' },
-                    })}
-                    placeholder="Author"
-                />
-                <p>{(errors['author']?.message) ? (String(errors['author']?.message)) : (
+                    )}
+                </p>
+                <TextField {...register('author')} placeholder="Author" />
+                <p>
+                    {errors['author']?.message ? (
+                        String(errors['author']?.message)
+                    ) : (
                         <></>
-                    )}</p>
+                    )}
+                </p>
                 <TextField
-                    // value={pages}
-                    // label="Pgs"
-                    // variant="outlined"
-                    // error={errorPages}
                     type="number"
-                    // onChange={(e) => {
-                    //     const value = e.target.value
-                    //     if (value === '' || value.match(/^[0-9]*$/)) {
-                    //         setPages(Number(value))
-                    //     }
-                    // }}
-                    {...register('pages', {
-                        required: 'Pages required',
-                        minLength: { value: 1, message: 'min. length 1' },
-                    })}
+                    {...register('pages')}
                     placeholder="Pages"
                 />
-                <p>{(errors['pages']?.message) ? (String(errors['pages']?.message)) : (
+                <p>
+                    {errors['pages']?.message ? (
+                        String(errors['pages']?.message)
+                    ) : (
                         <></>
-                    )}</p>
-                <TextField
-                    // value={genre}
-                    // label="Genre"
-                    // variant="outlined"
-                    // error={errorGenre}
-                    // onChange={(e) => setGenre(e.target.value)}
-                    {...register('genre', {
-                        required: 'Genre required',
-                        minLength: { value: 1, message: 'min. length 1' },
-                    })}
-                    placeholder="Genre"
-                />
-                <p>{(errors['genre']?.message) ? (String(errors['genre']?.message)) : (
+                    )}
+                </p>
+                <TextField {...register('genre')} placeholder="Genre" />
+                <p>
+                    {errors['genre']?.message ? (
+                        String(errors['genre']?.message)
+                    ) : (
                         <></>
-                    )}</p>
+                    )}
+                </p>
                 <TextField
-                    // value={price}
-                    // label="Price"
-                    // variant="outlined"
                     type="number"
-                    // error={errorPrice}
-                    // onChange={(e) => setPrice(Number(e.target.value))}
-                    {...register('price', {
-                        required: 'Price required',
-                        minLength: { value: 1, message: 'min. length 1' },
-                    })}
+                    {...register('price')}
                     placeholder="Price"
                 />
-               <p>{(errors['price']?.message) ? (String(errors['price']?.message)) : (
+                <p>
+                    {errors['price']?.message ? (
+                        String(errors['price']?.message)
+                    ) : (
                         <></>
-                    )}</p>
+                    )}
+                </p>
                 <TextField
-                    // value={stock}
-                    // label="Stock"
-                    // variant="outlined"
                     type="number"
-                    // error={errorStock}
-                    // onChange={(e) => {
-                    //     const value = e.target.value
-                    //     if (value === '' || value.match(/^[0-9]*$/)) {
-                    //         setStock(Number(value))
-                    //     }
-                    // }}
-                    {...register('stock', {
-                        required: 'Stock required',
-                        minLength: { value: 1, message: 'min. length 1' },
-                    })}
+                    {...register('stock')}
                     placeholder="Stock"
                 />
-                <p>{(errors['stock']?.message) ? (String(errors['stock']?.message)) : (
+                <p>
+                    {errors['stock']?.message ? (
+                        String(errors['stock']?.message)
+                    ) : (
                         <></>
-                    )}</p>
+                    )}
+                </p>
                 <Button variant="contained" size="large" type="submit">
                     Add Book
                 </Button>
