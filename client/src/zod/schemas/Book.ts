8255272aa28c3637fdd270573e7449d3ae9e9612
William Roberts <werobert@yahoo.com>
2024-10-22 10:37:14 -0400

    formatting

--- client/src/zod/schemas/Book.ts
+++ client/src/zod/schemas/Book.ts
@@ -1,30 +1,31 @@
 import { z } from 'zod'
 
-export const BookSchema = z
-    .object({
-        id: z.string().uuid().optional(),
-        title: z.string().min(1, { message: "Missing title" }),
-        author: z.string().min(1, { message: "Missing author" }),
-        pages: z.coerce
-            .number({
-                required_error: 'Pages is required',
-                invalid_type_error: 'Pages must be an integer > 0',
-            })
-            .int()
-            .positive().gte(1, { message: "Missing pages" }),
-        genre: z.string().min(1, { message: "Missing genre" }),
-        price: z.coerce
-            .number({
-                required_error: 'Price is required',
-                invalid_type_error: 'Price must be a number > 0',
-            })
-            .nonnegative().gte(1, { message: "Missing price" }),
-        stock: z.coerce
-            .number({
-                required_error: 'Stock is required',
-                invalid_type_error: 'Stock must be an integer >= 0',
-            })
-            .int()
-            .nonnegative().gte(1, { message: "Missing stock" }),
-    })
-    
+export const BookSchema = z.object({
+    id: z.string().uuid().optional(),
+    title: z.string().min(1, { message: 'Missing title' }),
+    author: z.string().min(1, { message: 'Missing author' }),
+    pages: z.coerce
+        .number({
+            required_error: 'Pages is required',
+            invalid_type_error: 'Pages must be an integer > 0',
+        })
+        .int()
+        .positive()
+        .gte(1, { message: 'Missing pages' }),
+    genre: z.string().min(1, { message: 'Missing genre' }),
+    price: z.coerce
+        .number({
+            required_error: 'Price is required',
+            invalid_type_error: 'Price must be a number > 0',
+        })
+        .nonnegative()
+        .gte(1, { message: 'Missing price' }),
+    stock: z.coerce
+        .number({
+            required_error: 'Stock is required',
+            invalid_type_error: 'Stock must be an integer >= 0',
+        })
+        .int()
+        .nonnegative()
+        .gte(1, { message: 'Missing stock' }),
+})
