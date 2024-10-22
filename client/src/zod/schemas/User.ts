e0f30020ea9cb052acd43a05fb2b6ba8e01ab7e2
William Roberts <werobert@yahoo.com>
2024-10-22 15:56:15 -0400

    pw regex input

--- client/src/zod/schemas/User.ts
+++ client/src/zod/schemas/User.ts
@@ -1,9 +1,15 @@
-import { z } from "zod";
+import { z } from 'zod'
+
+const pwRegex =
+    /^(?!\s+)(?!.*\s+$)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$^*.[\]{}()?"!@#%&/\\,><':;|_~`=+\- ])[A-Za-z0-9$^*.[\]{}()?"!@#%&/\\,><':;|_~`=+\- ]{8,256}$/
 
 export const UserSchema = z.object({
     userName: z.string().min(1, { message: 'Missing username' }),
-    password: z.string().min(8, { message: 'Password must be at least 8 chars. long' }),
+    password: z
+        .string()
+        .min(8, { message: 'PW must be at least 8 chars. long' })
+        .regex(pwRegex, {message: 'PW must be at least 8 chars long... etc'}),
     name: z.string().min(1, { message: 'Missing name' }),
     email: z.string().min(1, { message: 'Missing email' }),
-    phone: z.string().min(1, { message: 'Missing phone' })
+    phone: z.string().min(1, { message: 'Missing phone' }),
 })
